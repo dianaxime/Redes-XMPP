36186ff67ce354949f1a30603af57e0e768db1d3
Diana <dele18607@uvg.edu.gt>
2021-07-28 10:23:32 -0600

    change status

--- prueba.py
+++ prueba.py
@@ -3,6 +3,8 @@ import aiodns
 import asyncio
 import logging
 
+from prueba_status import Presence
+
 from slixmpp import ClientXMPP
 
 if sys.platform == 'win32' and sys.version_info >= (3, 8):
@@ -18,25 +20,41 @@ class EchoBot(ClientXMPP):
 
         self.add_event_handler("session_start", self.session_start)
         self.add_event_handler("message", self.message)
-
+        # self.add_event_handler("changed_status", self.wait_for_presences)
         # If you wanted more functionality, here's how to register plugins:
         # self.register_plugin('xep_0030') # Service Discovery
         # self.register_plugin('xep_0199') # XMPP Ping
 
+        # self.register_plugin('xep_0107') # User mood
         # Here's how to access plugins once you've registered them:
         # self['xep_0030'].add_feature('echo_demo')
 
-    def session_start(self, event):
-        self.send_presence()
-        self.get_roster()
 
+    def session_start(self, event):
+        self.send_presence('chat', 'hello my friends!')
+        self.get_roster()
+        
+
+        #print("---------------")
+        #print(event)
         # Most get_*/set_* methods from plugins use Iq stanzas, which
         # are sent asynchronously. You can almost always provide a
         # callback that will be executed when the reply is received.
 
     def message(self, msg):
+        print("........................")
+        print(msg)
         if msg['type'] in ('chat', 'normal'):
             msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+    def wait_for_presences(self, pres):
+        print("************")
+        print(pres)
+        self.received.add(pres['from'].bare)
+        if len(self.recieved)>=len(self.client_roster.keys()):
+            self.presences_received.set()
+        else:
+            self.presences_received.clear()       
 
 
 if __name__ == '__main__':
@@ -46,6 +64,6 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)-8s %(message)s')
 
-    xmpp = EchoBot('ddeleon@alumchat.xyz', '123')
+    xmpp = EchoBot('cas181202@alumchat.xyz', '123')
     xmpp.connect()
     xmpp.process()
