5837e2c7810e7857f668193a8c5501cde3767b59
Diana <dele18607@uvg.edu.gt>
2021-08-09 23:25:15 -0600

    advances in groups

---
+++ group.py
@@ -0,0 +1,79 @@
+import logging
+import threading
+import slixmpp
+import base64, time
+from slixmpp.exceptions import IqError, IqTimeout
+from slixmpp.xmlstream.stanzabase import ET, ElementBase 
+from getpass import getpass
+from argparse import ArgumentParser
+
+
+class JoinGroup(slixmpp.ClientXMPP):
+    def __init__(self, jid, password, room_jid, room_ak):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        #Handle events
+        self.add_event_handler("session_start", self.start)
+        self.room = room_jid
+        self.ak = room_ak
+
+    async def start(self, event):
+        #Send presence
+        self.send_presence()
+        await self.get_roster()
+        try:
+            #Join room
+            self.plugin['xep_0045'].join_muc(self.room, self.ak)
+            print("YOU ARE ON THE GROUP NOW")
+        except IqError as e:
+            print("Something went wrong", e)
+        except IqTimeout:
+            print("THE SERVER IS NOT WITH YOU")
+        self.disconnect()
+
+
+class ChatGroup(slixmpp.ClientXMPP):
+    def __init__(self, jid, password, room, nick):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.jid = jid
+        self.room = room
+        self.nick = nick
+
+        #event handlers
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("groupchat_message", self.muc_message)
+        self.add_event_handler("muc::%s::got_online" % self.room,
+                               self.muc_online)
+
+    async def start(self, event):
+        #Send events
+        await self.get_roster()
+        self.send_presence()
+        self.plugin['xep_0045'].join_muc(self.room,
+                                         self.nick,
+                                         # password=the_room_password,
+                                         )
+
+        #Message to write
+        message = input("Write the message: ")
+        self.send_message(mto=self.room,
+                          mbody=message,
+                          mtype='groupchat')
+
+    #Handle muc message
+    def muc_message(self, msg):
+        if(str(msg['from']).split('/')[1]!=self.nick):
+            print(str(msg['from']).split('/')[1] + ": " + msg['body'])
+            message = input("Write the message: ")
+            self.send_message(mto=msg['from'].bare,
+                              mbody=message,
+                              mtype='groupchat')
+
+    #Send message to group
+    def muc_online(self, presence):
+        if presence['muc']['nick'] != self.nick:
+            self.send_message(mto=presence['from'].bare,
+                              mbody="Hello, %s %s" % (presence['muc']['role'],
+                                                      presence['muc']['nick']),
+                              mtype='groupchat')
